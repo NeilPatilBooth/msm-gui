9bac211d1d425a6a20f831f663d2af4d086a5409
NeilPatilBooth <npatil@ChicagoBooth.edu>
2024-11-14 22:29:28 +0000

    actor create form works

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,11 +1,34 @@
 class ActorsController < ApplicationController
+  
+  def create
+    #Paramters look like:
+    #Parameters: {"query_name"=>"Neil", "query_dob"=>"1994-03-01", "query_bio"=>"Yay", "query_image"=>""}
+
+    a = Actor.new
+    a.name = params.fetch("query_name")
+    a.dob = params.fetch("query_dob")
+    a.bio = params.fetch("query_bio")
+    a.image = params.fetch("query_image")
+    
+    a.save 
+
+    redirect_to("/actors")
+    #Retrieve user's inputs from params
+    #Create a record in the actor table 
+    #Populate each column with the user input 
+    #Redirect the user back to the /movies URL
+  end
+  
+  
+  
+  
   def index
     matching_actors = Actor.all
     @list_of_actors = matching_actors.order({ :created_at => :desc })
 
     render({ :template => "actor_templates/index" })
   end
-#howdy
+
   def show
     the_id = params.fetch("path_id")
 
